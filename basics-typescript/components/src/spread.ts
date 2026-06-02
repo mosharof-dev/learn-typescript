b1a25adce20d0eea94c50d9607a1f4382c3a1228
mosharof-dev <mosharof.dev@gmail.com>
2026-06-02 22:30:50 +0600

    Add example of spread operator usage in TypeScript

---
+++ basics-typescript/components/src/spread.ts
@@ -0,0 +1,15 @@
+// Spread Operator
+
+// rest operator
+
+// Destructuring in typescript
+
+
+
+// learn spread operator in typescript
+const friend: string[] = ['Rafin', 'Rakib','Tanvir'];
+
+const user: string[] = ['Kolim', 'Solim', 'Korim'];
+
+friend.push(...user)
+
